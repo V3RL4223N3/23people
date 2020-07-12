dd1c7cb5266c82c901f5bbeedbd38de454da8cc9
Daniel <danielf.tapiar@gmail.com>
2020-07-11 21:25:37 -0400

    added curl command to verify api deployment

--- deploy-app.sh
+++ deploy-app.sh
@@ -5,3 +5,8 @@ docker push snbsniper29/23people:prod
 
 docker run -p 80:80 --net=host -d snbsniper29/23people:prod
 
+
+#API-HEADER: apikey
+#API-KEY: GU5Ldi5IvmAxihA13RV2AKQkQui9cL7d
+curl --location --request GET 'https://danielftapiar-eval-prod.apigee.net/people?apikey=GU5Ldi5IvmAxihA13RV2AKQkQui9cL7d' \
+--header 'Cookie: REVEL_FLASH='
